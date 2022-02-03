79f0a3d602e2f19d1a15028201da8a51d3e21237
prawinkumar6655 <98937586+prawinkumar6655@users.noreply.github.com>
2022-02-03 15:31:41 +0530

    Update App.java

--- src/main/java/cts/App.java
+++ src/main/java/cts/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "HI guys this is kamal sir training" );
     }
 }
